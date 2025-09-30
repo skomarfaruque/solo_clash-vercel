5ded996f13fa3ff0ad53fc33000ef53cffab2571
skomarfaruque <omar.faruque@ntb.no>
2025-09-30 20:13:26 +0600

    style: update className for SubscriptionSectionCardNew to improve layout consistency style: clean up imports in SubscriptionsSectionNew for better readability

--- src/components/account/SubscriptionSectionCardNew.tsx
+++ src/components/account/SubscriptionSectionCardNew.tsx
@@ -19,7 +19,7 @@ export default function SubscriptionSectionCardNew({
 }: SubscriptionCardProps) {
   return (
     <div
-      className="rounded-2xl hover:scale-105 transition relative w-full max-w-[323px] h-[498px] sm:h-[498px] bg-cover bg-center bg-no-repeat hover:!bg-[url('/new_subscription_bg-active.svg')]"
+      className="rounded-2xl hover:scale-105 transition relative w-full max-w-full sm:max-w-[323px] h-[498px] sm:h-[498px] bg-cover bg-center bg-no-repeat hover:!bg-[url('/new_subscription_bg-active.svg')]"
       style={{
         backgroundImage: `url('/new_subscription_bg.svg')`,
       }}
